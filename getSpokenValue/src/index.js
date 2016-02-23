8ef8d0887149886533e551a0c1d1354805dd88db
bill rowland <bill.rowland@bcbsnc.com>
2016-02-23 16:51:57 -0500

    added getSpokenValues

---
+++ getSpokenValue/src/index.js
@@ -0,0 +1,87 @@
+
+/**
+ * dad: an alexa app to tell you the day of the week (in greenwich london)
+ *
+ * main speech threads:
+ * what day is it
+ *
+ * Dialog model:
+ *  User: "Alexa, ask ozzi what day is it"
+ *  Alexa: "monday"
+ *  
+ * note: the reason we're using GMT is that node servers report the date as GMT, and local
+ * timezones/time are not available to external developers
+ */
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined;//replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+var DAYS_OF_WEEK = [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday' ];
+
+// The AlexaSkill prototype and helper functions 
+var AlexaSkill = require('./AlexaSkill');
+
+// setup patternSkill
+var patternSkill = function () {
+	AlexaSkill.call(this, APP_ID);
+};
+
+patternSkill.prototype = Object.create(AlexaSkill.prototype);
+patternSkill.prototype.constructor = patternSkill;
+
+// default intent
+ patternSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+ 	session.attributes.msgText = "nothing here, move along.";
+	mainIntent(intent, session, response);
+};
+
+// intent handlers
+
+patternSkill.prototype.intentHandlers = {
+	"MainIntent": function (intent, session, response) {
+		mainIntent(intent, session, response);
+	},
+
+	"AMAZON.HelpIntent": function (intent, session, response) {
+		var speechOutput = {
+			speech: session.attributes.msgText,
+			type: AlexaSkill.speechOutputType.PLAIN_TEXT
+		};
+		var repromptOutput = {
+			speech: session.attributes.msgText,
+			type: AlexaSkill.speechOutputType.PLAIN_TEXT
+		};
+		// For the repromptText, play the speechOutput again
+		response.ask(speechOutput, repromptOutput);
+	},
+	"AMAZON.StopIntent": function (intent, session, response) {
+		response.tell(session.attributes.msgText);
+	},
+
+	"AMAZON.CancelIntent": function (intent, session, response) {
+		response.tell(session.attributes.msgText);
+	}
+};
+
+// figure out the day in greenwich, london and output it
+
+function mainIntent(intent, session, response) {
+	var interpretedValue = intent.slots.randomWords.value;
+	console.log('----------------- ', interpretedValue, ' ----------------');
+	var speechText = "i'm all done.";
+
+	var speechOutput = {
+		speech: speechText,
+		type: AlexaSkill.speechOutputType.PLAIN_TEXT
+	};
+
+	response.tellWithCard(speechOutput, "get spoken value", intent.slots.randomWords.value);
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+	// Create an instance of the  patternSkill.
+	var skill = new patternSkill();
+	skill.execute(event, context);
+};
